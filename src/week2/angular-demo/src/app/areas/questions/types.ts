9651ab002fbcedd13584aced64b9b4dbade43642
Jeffry Gonzalez <jeff@hypertheory.com>
2026-02-19 15:58:02 -0500

    big jump

--- src/week2/angular-demo/src/app/areas/questions/types.ts
+++ src/week2/angular-demo/src/app/areas/questions/types.ts
@@ -7,3 +7,14 @@ export type QuestionListItem = {
     content: string;
   }[];
 };
+
+/*    public record QuestionSubmissionItem
+    {
+        [MinLength(5), MaxLength(100)] public required string Title { get; set; } = string.Empty;
+        [MinLength(10), MaxLength(1000)] public required string Content { get; set; } = string.Empty;
+    }*/
+
+export type QuestionSubmissionItem = {
+  title: string;
+  content: string;
+};
